0e1de2e9f8b0364774aa4ca1935ec307623c7ed6
Vitor Costa Salem <vitor635@hotmail.com>
2023-09-10 19:26:54 -0300

    Add project

---
+++ src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        nome("Vitor Costa Salem");
+
+        cursoPucMinas();
+    }
+
+    public static void nome(String nome){
+        // Metodo utilizado para imprimir o nome passado como parametro
+        // Aprendizado para novo programandores
+
+
+        System.out.println(nome);
+
+
+    }
+
+    public static void cursoPucMinas(){
+        // Metodo utilizado para imprimir o nome do curso de Engenharia de Software PUC praca
+        // Aprendizado para novo programandores
+
+
+        System.out.println("Engenharia de Software");
+
+
+
+
+        
+    }
+
+}
